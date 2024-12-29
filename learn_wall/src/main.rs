cf4f5f6d8bfd51066b4920c65c31865e53b707ca
Yassir Karroum <ukarroum17@gmail.com>
2024-12-29 12:45:00 +0100

    Add code to retrieve an entity id from api

--- learn_wall/src/main.rs
+++ learn_wall/src/main.rs
@@ -1,3 +1,18 @@
+extern crate reqwest;
+use reqwest::header;
+
+const API_ENDPOINT: &str = "https://apicollections.parismusees.paris.fr/graphql";
+
 fn main() {
-    println!("Hello, world!");
+    let mut headers = header::HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+    headers.insert("auth-token", "API_KEY".parse().unwrap());
+
+    let client = reqwest::blocking::Client::builder().build().unwrap();
+
+    let res = client.post(API_ENDPOINT).headers(headers).body("{\"query\": \"{  nodeQuery(     filter: {}, offset: 3, limit: 1   ) {     entities {       entityUuid     }   } }\"}")
+        .send().unwrap()
+        .text().unwrap();
+
+    println!("{}", res);
 }
