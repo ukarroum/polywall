8d1e40a23c91763f5d281f511f86732a45c5c46c
Yassir Karroum <ukarroum17@gmail.com>
2024-12-30 13:15:28 +0100

    Added query to retrieve entity details

--- learn_wall/src/main.rs
+++ learn_wall/src/main.rs
@@ -10,9 +10,22 @@ fn main() {
 
     let client = reqwest::blocking::Client::builder().build().unwrap();
 
-    let res = client.post(API_ENDPOINT).headers(headers).body("{\"query\": \"{  nodeQuery(     filter: {}, offset: 3, limit: 1   ) {     entities {       entityUuid     }   } }\"}")
+    let res = client.post(API_ENDPOINT).headers(headers.clone()).body("{\"query\": \"{  nodeQuery(     filter: {conditions: [{field: \\\"field_oeuvre_types_objet.entity.field_lref_adlib\\\", value: \\\"4284\\\"}]}, offset: 3, limit: 1   ) {     entities {       entityUuid     }   } }\"}")
         .send().unwrap()
         .text().unwrap();
+
+    println!("{}", res);
+
+    let start = "{\"data\":{\"nodeQuery\":{\"entities\":[{\"entityUuid\":\"".len();
+    let uuid = &res[start..start + 36];
+
+    let query = format!("{{\"query\": \"{{   nodeQuery(     filter: {{conditions: [{{field: \\\"uuid\\\", value: \\\"{uuid}\\\"}}]}}   ) {{     entities {{       entityUuid       ... on NodeOeuvre {{         title         fieldVisuels {{           entity {{             publicUrl           }}         }}       }}     }}   }} }}\"}}");
+    println!("{}", query);
+
+    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(query)
+        .send().unwrap()
+        .text().unwrap();
+
 
     println!("{}", res);
 }
