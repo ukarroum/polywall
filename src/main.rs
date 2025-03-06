b67c7ceb0c7ac98ffb5e98bd22506bf555edff43
Yassir Karroum <ukarroum17@gmail.com>
2025-03-06 12:50:48 +0100

    Removed unecessary second request

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,14 @@ const API_ENDPOINT: &str = "https://apicollections.parismusees.paris.fr/graphql"
 fn main() {
     let mut headers = header::HeaderMap::new();
     headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("auth-token", "API_KEY".parse().unwrap());
+    headers.insert("auth-token", "c4a91329-0317-42b0-94c5-9fa91b1360c7".parse().unwrap());
 
     let client = reqwest::blocking::Client::builder().build().unwrap();
 
-    let res = client.post(API_ENDPOINT).headers(headers.clone()).body("{\"query\": \"{  nodeQuery(     filter: {conditions: [{field: \\\"field_oeuvre_types_objet.entity.field_lref_adlib\\\", value: \\\"4284\\\"}]}, offset: 3, limit: 1   ) {     entities {       entityUuid     }   } }\"}")
+    // 4284 => 
+    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(r#"{"query": "{  nodeQuery(     filter: {conditions: [{field: \"field_oeuvre_types_objet.entity.field_lref_adlib\", value: \"4284\"}]}, offset: 3, limit: 1   ) {     entities {       entityUuid ... on NodeOeuvre { title fieldVisuels { entity {publicUrl}} }     }   } }"}"#)
         .send().unwrap()
         .text().unwrap();
-
-    println!("{}", res);
-
-    let start = "{\"data\":{\"nodeQuery\":{\"entities\":[{\"entityUuid\":\"".len();
-    let uuid = &res[start..start + 36];
-
-    let query = format!("{{\"query\": \"{{   nodeQuery(     filter: {{conditions: [{{field: \\\"uuid\\\", value: \\\"{uuid}\\\"}}]}}   ) {{     entities {{       entityUuid       ... on NodeOeuvre {{         title         fieldVisuels {{           entity {{             publicUrl           }}         }}       }}     }}   }} }}\"}}");
-    println!("{}", query);
-
-    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(query)
-        .send().unwrap()
-        .text().unwrap();
-
 
     println!("{}", res);
 }
