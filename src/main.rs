62129ef38ccbe7aefd9407bf7b9f44bf86252c93
Yassir Karroum <ukarroum17@gmail.com>
2025-03-09 11:09:36 +0100

    Add filter on license

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ const API_ENDPOINT: &str = "https://apicollections.parismusees.paris.fr/graphql"
 fn main() {
     let mut headers = header::HeaderMap::new();
     headers.insert("Content-Type", "application/json".parse().unwrap());
-    headers.insert("auth-token", "c4a91329-0317-42b0-94c5-9fa91b1360c7".parse().unwrap());
+    headers.insert("auth-token", "API_KEY".parse().unwrap());
 
     let client = reqwest::blocking::Client::builder().build().unwrap();
 
-    // 4284 => 
-    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(r#"{"query": "{  nodeQuery(     filter: {conditions: [{field: \"field_oeuvre_types_objet.entity.field_lref_adlib\", value: \"4284\"}]}, offset: 3, limit: 1   ) {     entities {       entityUuid ... on NodeOeuvre { title fieldVisuels { entity {publicUrl}} }     }   } }"}"#)
+    // 4284 => Paiting
+    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(r#"{"query": "{ nodeQuery( filter: {conditions: [{field: \"field_oeuvre_types_objet.entity.field_lref_adlib\", value: \"4284\"}, {field: \"field_visuels.entity.field_image_libre\", value: \"1\"}]}, offset: 3, limit: 1) { entities { ... on NodeOeuvre { title fieldVisuels { entity {publicUrl}} }}}}"}"#)
         .send().unwrap()
         .text().unwrap();
 
