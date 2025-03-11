e3d3e1f94f1bfe5bfa4f381eb048a411f835f22b
Yassir Karroum <ukarroum17@gmail.com>
2025-03-11 08:00:47 +0100

    Offset is now randomly generated

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use regex::Regex;
 use std::process::Command;
 use std::env;
 use std::string::String;
+use rand::Rng;
 
 const API_ENDPOINT: &str = "https://apicollections.parismusees.paris.fr/graphql";
 
@@ -17,7 +18,24 @@ fn main() {
     let client = reqwest::blocking::Client::builder().build().unwrap();
 
     // 4284 => Paiting
-    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(r#"{"query": "{ nodeQuery( filter: {conditions: [{field: \"field_oeuvre_types_objet.entity.field_lref_adlib\", value: \"4284\"}, {field: \"field_visuels.entity.field_image_libre\", value: \"1\"}]}, offset: 17, limit: 1) { entities { ... on NodeOeuvre { title fieldVisuels { entity {publicUrl}} }}}}"}"#)
+    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(r#"{"query": "{ nodeQuery( filter: {conditions: [{field: \"field_oeuvre_types_objet.entity.field_lref_adlib\", value: \"4284\"}, {field: \"field_visuels.entity.field_image_libre\", value: \"1\"}]}) { count }}"}"#)
+        .send().unwrap()
+        .text().unwrap();
+
+    let re = Regex::new("count\":(?<count>.*?)}").unwrap();
+
+    let Some(json) = re.captures(&res) else {
+        println!("No match");
+        return;
+    };
+
+    let count: i64 = json["count"].parse().unwrap();
+
+
+    let mut rng = rand::rng();
+    let offset = rng.random_range(0..count);
+
+    let res = client.post(API_ENDPOINT).headers(headers.clone()).body(r#"{"query": "{ nodeQuery( filter: {conditions: [{field: \"field_oeuvre_types_objet.entity.field_lref_adlib\", value: \"4284\"}, {field: \"field_visuels.entity.field_image_libre\", value: \"1\"}]}, offset: {offset}, limit: 1) { entities { ... on NodeOeuvre { title fieldVisuels { entity {publicUrl}} }}}}"}"#.replace(r"{offset}", &offset.to_string()))
         .send().unwrap()
         .text().unwrap();
 
